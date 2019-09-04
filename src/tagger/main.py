c9cf984ab25b86e87f6440f52927e47f76c5bcfa
Matmorcat <mattmoretz@yahoo.com>
2019-09-04 03:30:21 -0400

    Automatically grab storms from website

--- src/tagger/main.py
+++ src/tagger/main.py
@@ -1,3 +1,5 @@
+import re
+
 URL_BASE = 'https://storms.ngs.noaa.gov/'
 URL_STORMS = URL_BASE + 'storms/'
 
@@ -7,8 +9,17 @@ try:
     r = requests.get(URL_BASE)
     if r.status_code == requests.codes.ok:
         print('Connection successful!')
+
+        list_storms = dict()
+
+        for storm_id, storm_name in re.findall("<a href=\"https://geodesy\\.noaa\\.gov/storm_archive/storms/([^/]+)/index\\.html\">(.*)</a>", r.text):
+            list_storms[storm_id] = storm_name
+
+        print()
+        for storm in list_storms:
+            print(list_storms[storm])
     else:
         print('Connection failed! Returned code: ' + r.status_code)
-except:
+except Exception as e:
     print('Error occurred while trying to connect to ' + URL_BASE)
-LIST_STORMS = {''}
+    print('Error: ' + str(e))
