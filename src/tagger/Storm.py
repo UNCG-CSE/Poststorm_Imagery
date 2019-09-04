2c26ae3e9b4da86c566099e9e60f4120f7bbf79a
Matmorcat <mcmoretz@uncg.edu>
2019-09-04 10:50:18 -0400

    Modify Storm tar variable declarations

--- src/tagger/Storm.py
+++ src/tagger/Storm.py
@@ -1,8 +1,15 @@
+import tarfile
+from typing import List
+
+
 class Storm:
 
     storm_id: str
     storm_title: str
     storm_year: int
+
+    tar_file: tarfile.TarFile
+    tar_index: List[tarfile.TarInfo]
 
     def __init__(self, storm_id: str, storm_title: str, storm_year):
         self.storm_id = storm_id
@@ -11,3 +18,13 @@ class Storm:
 
     def __str__(self):
         return self.storm_title + '(' + str(self.storm_year) + ')'
+
+    def load_data(self, tar_url: str):
+        """
+
+        Load an archive (.tar) into memory
+        :param tar_url: str
+        """
+        # Open the tar file for reading with transparent compression
+        self.tar_file = tarfile.open(tar_url, 'r')
+        self.tar_index = self.tar_file.getmembers()
