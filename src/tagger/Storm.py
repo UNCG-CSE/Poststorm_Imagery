6181ea2e3a24e4ec648d8494f510a76e6064d282
Matmorcat <mattmoretz@yahoo.com>
2019-09-04 04:03:08 -0400

    Change year's type to an integer

--- src/tagger/Storm.py
+++ src/tagger/Storm.py
@@ -2,12 +2,12 @@ class Storm:
 
     storm_id: str
     storm_title: str
-    storm_year: str
+    storm_year: int
 
     def __init__(self, storm_id: str, storm_title: str, storm_year):
         self.storm_id = storm_id
         self.storm_title = storm_title
-        self.storm_year = storm_year
+        self.storm_year = int(storm_year)
 
     def __str__(self):
-        return self.storm_title + '(' + self.storm_year + ')'
+        return self.storm_title + '(' + str(self.storm_year) + ')'
