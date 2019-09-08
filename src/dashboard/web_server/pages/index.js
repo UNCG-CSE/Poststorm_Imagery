727c4bf63206dbac00785776df98d4a57d352f6e
ShahNafisRafiqu <sarafiqu@uncg.edu>
2019-09-07 23:54:45 -0400

    added errror checking for image retrival and combined node server and webserver into one

--- src/dashboard/web_server/pages/index.js
+++ src/dashboard/web_server/pages/index.js
@@ -2,43 +2,56 @@ import Layout from '../src/components/layout'
 import Fetch from 'isomorphic-unfetch';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
+const SAD_FACE =`
+https://www.nationwidechildrens.org/-/media/nch/giving/images/on-our-sleeves-1010/icons/icon-teasers/w45084_iconcollectionlandingiconteaserimages_facesad.jpg
+`
+
 const Index = (props) => (
     
-        <Layout >
-          
-            <center>
-                <MDBCol className="pt-2">
-                    <MDBCard style={{ width: "auto" }} className="stylish-color-dark">
-
-                        <MDBCardImage className="w-75 img-fluid pt-4 pb-4 px-2" src={props.file_url} waves />
-                    
-                        <MDBCardBody >
-                            <MDBCardTitle className="white-text h3">{props.file_name}</MDBCardTitle>
-                            <MDBCardText className="white-text">
-                                <b>
-                                    <u>
-                                        Wowe api call
-                                    </u>
-                                </b>
-
-                                <p className='red-text'>
-                                {JSON.stringify(props,null,8)}
-                                </p>
-                            </MDBCardText>
-                            <MDBBtn href="register">MDBBtn</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </center>
-            
-        </Layout>
+    <Layout >
+        <center>
+            <MDBCol className="pt-2">
+                <MDBCard style={{ width: "auto" }} className="stylish-color-dark">
+                    <MDBCardImage className="w-75 img-fluid pt-4 pb-4 px-2" src={props.file_url || SAD_FACE} alt="The big sad"waves />
+                    <MDBCardBody >
+                        <MDBCardTitle className="white-text h3">{props.file_name}</MDBCardTitle>
+                        <MDBCardText className="white-text">
+                            <b>
+                                <u>
+                                    Wowe api call
+                                </u>
+                            </b>
+                            <p className='green-text'>
+                            {JSON.stringify(props,null,8)}
+                            </p>
+                        </MDBCardText>
+                        <MDBBtn href="register">I dont do anything yet,sorta</MDBBtn>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+        </center> 
+    </Layout>
     
 )
 
 Index.getInitialProps = async function() {
-    const IP='35.239.226.117'//'192.168.56.1';
-    const res = await fetch('http://'+IP+':4000/')
-    const data = await res.json()
+    const IP='192.168.56.1'//'35.239.226.117'//'192.168.56.1';
+    const API_URL='http://'+IP+':3000/get_image'
+    //Default data incase Fetch fails. 
+    let data = {
+        file_url:undefined,
+        file_name:undefined,
+        api_url:API_URL
+    }
+
+    
+    const res = await fetch(API_URL).then(async function() {
+        console.log("Image fetched");
+        data = await res.json()
+    }).catch(function() {
+        console.log("Failed to fetch image");
+    });
+      
     return data
 }
 
--- src/dashboard/web_server/server.js
+++ src/dashboard/web_server/server.js
@@ -2,23 +2,92 @@
 const express = require('express')
 const app_express = express();
 const next = require('next')
+const path = require('path');
+const fs = require('fs');
 
 const PORT=3000
+const DIR = path.join(__dirname, '');
+const DIR_DATA = path.join(__dirname, 'data');
+const IP='192.168.56.1:'+PORT
+const IMAGE_ROUTE='photos'
+const IMAGE_FOLDER='data'
 
 const dev = process.env.NODE_ENV !== 'production'
 const app_next = next({ dev })
 const handle = app_next.getRequestHandler()
 
+//This variable to used to keep track of all images that have a route.
+let image_list=[];
+
 //---START---
 app_next.prepare()
 .then(() => {
-
-    //For any requests, let Next.Js handle it
-    app_express.get('*', (req, res) => {
-        return handle(req, res)
+    
+    //Test Api for testing
+    app_express.get('/test_api', (req, res) => {
+        res.json(
+            {
+                test_api:'wowe test api',
+                test_port:PORT,
+                test_rng:Math.random()
+            }
+        )
     })
+    
+    //Generate routs for all images in file
+    fs.readdir(DIR_DATA, function (err, files) {
+        //handling error,??? does this even work lol
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
 
-    //have the server listen on PORT and use machines IP>
+        //listing all files using forEach
+        files.forEach(function (file) {
+        
+            //Get the file name without extension.
+            const file_name=file.split('.').slice(0, -1).join('.')
+            const file_ext=file.split('.')[1]
+            const file_route='/'+IMAGE_ROUTE+'/'+file_name
+            const file_path=IMAGE_FOLDER+'/'+file
+            const file_url='http://'+IP+file_route
+
+            //Add that to the list of images with routes
+            let image_route={
+                file_name:file_name,
+                file_ext:file_ext,
+                file_route:file_route,
+                file_url:file_url
+            }
+            image_list.push(image_route)
+            //console.log(image_route)
+            //Create a route with that images name,and send that file.
+            app_express.get(file_route, (req, res, next) => {
+                res.sendFile(path.resolve(path.resolve(__dirname,file_path)));
+            });
+        });
+
+        //Make a route to get a random image
+        app_express.get('/get_image', (req, res) => {
+            const selected_img=image_list[Math.floor(Math.random()*image_list.length)]
+            res.json(
+                selected_img
+            )
+        })
+
+        //For any requests, let Next.Js handle it. 
+        //Put this after all custom routes so that Next js knows not to error that route
+        app_express.get('*', (req, res) => {
+            return handle(req, res)
+        })
+       
+        //show_routes()
+        
+    });
+
+    
+
+    
+    //have the server listen on PORT and use machines IP
     app_express.listen(PORT,'0.0.0.0', (err) => {
         console.log(`> Ready on http://localhost:${PORT}`)
     });
@@ -27,3 +96,8 @@ app_next.prepare()
     console.error(ex.stack)
     process.exit(1)
 })
+
+async function show_routes()
+{   
+    console.log(app_express._router.stack)
+}
