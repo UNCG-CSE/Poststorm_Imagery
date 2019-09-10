33350da1482cab0e3f3c757a74fd0a0ac2592c7d
Matmorcat <mattmoretz@yahoo.com>
2019-09-10 04:10:11 -0400

    Remove incomplete test case

--- Poststorm_Imagery/tests/test_connectionHandler.py
+++ Poststorm_Imagery/tests/test_connectionHandler.py
@@ -5,5 +5,5 @@ class TestConnectionHandler(TestCase):
     def test_generate_storm_list(self):
         self.generate_storm_list()
 
-    def test_get_storm_list(self):
-        self.fail()
+    #def test_get_storm_list(self):
+    #    self.fail()
