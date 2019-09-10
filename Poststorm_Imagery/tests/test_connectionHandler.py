91369893581c8cbac1420cc200f42e081e26a3ea
Matmorcat <mattmoretz@yahoo.com>
2019-09-10 04:12:37 -0400

    Fix another unused test case

--- Poststorm_Imagery/tests/test_connectionHandler.py
+++ Poststorm_Imagery/tests/test_connectionHandler.py
@@ -2,8 +2,9 @@ from unittest import TestCase
 
 
 class TestConnectionHandler(TestCase):
-    def test_generate_storm_list(self):
-        self.generate_storm_list()
+    #def test_generate_storm_list(self):
+    #    self.generate_storm_list()
 
     #def test_get_storm_list(self):
     #    self.fail()
+    pass
