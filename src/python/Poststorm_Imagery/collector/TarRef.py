c787cd017f12747e6819d1ac1937b7d9648d7cc0
Matmorcat <mattmoretz@yahoo.com>
2019-09-12 02:54:56 -0400

    Update lock with total size

--- src/python/Poststorm_Imagery/collector/TarRef.py
+++ src/python/Poststorm_Imagery/collector/TarRef.py
@@ -168,7 +168,9 @@ class TarRef:
                                  total=ceil((remaining_size + local_size) / chunk_size),
                                  initial=ceil(local_size / chunk_size), unit=unit, miniters=1):
                     if (datetime.now() - last_lock_update).total_seconds() > 60:  # 1800 seconds = 30 minutes
-                        helpers.update_file_lock(part_file=tar_file_path_part, user=user, part_size_byte=os.path.getsize(tar_file_path_part))
+                        helpers.update_file_lock(part_file=tar_file_path_part, user=user,
+                                                 part_size_byte=os.path.getsize(tar_file_path_part),
+                                                 total_size_byte=full_size_origin)
                     f.write(data)
             except:
                 raise
