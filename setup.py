7f816da802f8f436cb51aff2834e31af7b713370
Matmorcat <mattmoretz@yahoo.com>
2019-09-12 02:23:31 -0400

    0.5.3 -> 0.5.4

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Matmorcat/Poststorm_Imagery',
-    version='0.5.3',
+    version='0.5.4',
     zip_safe=False,
 )
