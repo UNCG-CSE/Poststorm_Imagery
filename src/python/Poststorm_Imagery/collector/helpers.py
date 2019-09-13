15696f575d4dfe4294871b171e9ad8733ea6c4fa
Matmorcat <mcmoretz@uncg.edu>
2019-09-13 02:55:52 -0400

    Add protection against unknown values in printing readable size

--- src/python/Poststorm_Imagery/collector/helpers.py
+++ src/python/Poststorm_Imagery/collector/helpers.py
@@ -4,6 +4,10 @@ from typing import Union, Dict
 
 
 def get_byte_size_readable(byte_count: int) -> str:
+
+    if type(byte_count) != int:
+        return "??? Bytes"
+
     if byte_count < 2 ** 20:  # MiB = 2^20
         return str(round(byte_count / 2 ** 10, 2)) + ' KiBs'
     if byte_count < 2 ** 30:  # GiB = 2^30
