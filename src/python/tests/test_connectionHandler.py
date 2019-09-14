e8bb63c81f97d9b3932dce6ab0d77cd18fd986ee
Matmorcat <mattmoretz@yahoo.com>
2019-09-14 00:46:03 -0400

    Generify response to just the text, since nothing else is needed and it makes unit testing easier

--- src/python/tests/test_connectionHandler.py
+++ src/python/tests/test_connectionHandler.py
@@ -1,10 +1,39 @@
 from unittest import TestCase
 
+from collector.ConnectionHandler import ConnectionHandler
+
 
 class TestConnectionHandler(TestCase):
-    #def test_generate_storm_list(self):
-    #    self.generate_storm_list()
 
-    #def test_get_storm_list(self):
-    #    self.fail()
-    pass
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.c = ConnectionHandler(html_text=open('resources/Storms_List_page.html', 'r').read())
+
+    def test_generate_storm_list_all(self):
+
+        # Parse all 34 storms
+        self.assertEqual(len(self.c.generate_storm_list()), 34)
+
+    def test_generate_storm_list_search_year(self):
+
+        # Parse all 2008 storms (2)
+        self.assertEqual(len(self.c.generate_storm_list(search_re='2008')), 2)
+
+    def test_generate_storm_list_search_name_term(self):
+
+        # Parse all hurricanes (25)
+        self.assertEqual(len(self.c.generate_storm_list(search_re='hurricane')), 25)
+
+    def test_generate_storm_list_search_none(self):
+
+        # Test when there are no matches
+        self.assertEqual(len(self.c.generate_storm_list(search_re='no storms will match this')), 0)
+
+    def test_get_storm_list(self):
+        pass
+        """
+        storms: List[Storm] = c.get_storm_list(OPTIONS.storm)
+
+        for storm in c.storms
+        self.assertEqual(, 'unfilled')
+        """
