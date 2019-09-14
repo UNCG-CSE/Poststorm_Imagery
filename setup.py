dab3ccf4e48d6206af10ed85035967634257ef00
Matmorcat <mattmoretz@yahoo.com>
2019-09-14 07:33:40 -0400

    Generic fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 # with open('README.md') as readme_file:
 #     readme = readme_file.read()
 
-requirements: list()
+requirements: []
 
 with open('./requirements.txt') as reqs:
     requirements = reqs.read().splitlines()
