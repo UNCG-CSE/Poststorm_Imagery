26f1691a011eab2cfd120ef749361eecd8977b9b
Rinty <r_chowdh@uncg.edu>
2019-09-17 14:02:28 -0400

    Basic framework for resizing jpg images

---
+++ src/python/Poststorm_Imagery/shrink/resize.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+#declair the size to convert the image
+size_300 = (300,300)
+
+#spath = r'G:\\Shared drives\\C-Sick\\data'
+#print(os.listdir(spath))
+
+#go through each image in the directory and find the .jgp file and resize them and save them to folder named 'mine'
+for f in os.listdir('.'):
+    if f.endswith('.jpg'):
+        i = Image.open(f)
+        fn, fext = os.path.splitext(f)
+
+        i.thumbnail(size_300)
+        i.save('mine/{}_300{}'.format(fn, fext))
