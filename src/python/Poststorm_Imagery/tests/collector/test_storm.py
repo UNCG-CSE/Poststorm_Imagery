bc095834350f5c0841b184b50d0d2bb3b984d21c
Matmorcat <mattmoretz@yahoo.com>
2019-09-30 10:06:08 -0400

    Add tests for generating tar list

--- src/python/Poststorm_Imagery/tests/collector/test_storm.py
+++ src/python/Poststorm_Imagery/tests/collector/test_storm.py
@@ -9,8 +9,18 @@ class TestStorm(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.storm = Storm(os.path.join(s.URL_STORMS, "michael"), "michael", "Hurricane Michael", 2018)
+        cls.storm = Storm(s.URL_STORMS + "michael/index.html", "michael", "Hurricane Michael", 2018)
 
     def test_valid_string(self):
         self.assertIn("Hurricane Michael", str(self.storm))
         self.assertIn("(2018)", str(self.storm))
+
+    def test_generate_tar_list_full(self):
+        self.storm.generate_tar_list()
+        self.assertGreaterEqual(len(self.storm.tar_list), 10)
+        self.assertEquals(str(self.storm.tar_list_last_pattern), '.*')
+
+    def test_generate_tar_list_rgb(self):
+        self.storm.generate_tar_list(search_re="RGB")
+        self.assertGreaterEqual(len(self.storm.tar_list), 5)
+        self.assertEquals(str(self.storm.tar_list_last_pattern), 'RGB')
