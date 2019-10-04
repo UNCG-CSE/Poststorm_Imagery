11031959a2b754cda5efa2a8e6c03d868bcdad06
ShahNafisRafique <sarafiqu@uncg.edu>
2019-10-04 00:38:45 -0400

    Initial commit of basic login/reg
    just visual,no server yet

---
+++ src/dashboard/auth/pages/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Layout from '../src/components/layouts/layout'
+import LoginRegisterLayout from '../src/components/layouts/loginRegisterLayout'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
+export default function Index() {
+  return (
+    <Layout>  
+      <LoginRegisterLayout type='login'/>   
+    </Layout>
+  );
+}
