c669a6f7e429d74204f838b9761998533bcbfcc1
Shah Nafis Rafique <sarafiqu@uncg.edu>
2019-10-04 21:32:05 -0400

    created a template example

---
+++ src/dashboard/auth_template/pages/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const useStyles = makeStyles(theme => ({
+  
+}));
+
+export default function Index() {
+  const classes = useStyles();
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
