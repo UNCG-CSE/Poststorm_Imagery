19d62c8ec7b460c5abb705b50bcaa338ec426099
Matmorcat <mattmoretz@yahoo.com>
2019-10-10 03:22:22 -0400

    Put it in a pickle

--- src/python/Poststorm_Imagery/assigner/assign.py
+++ src/python/Poststorm_Imagery/assigner/assign.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-import pickle
+import jsonpickle
 from typing import Union, Set
 
 from Poststorm_Imagery.assigner.image_assigner import ImageAssigner
@@ -18,19 +18,19 @@ SMALL_TAR_CACHE_PATH: Union[bytes, str] = os.path.join(SMALL_PATH, s.TAR_CACHE)
 # Define command-line parameters and arguments #
 ################################################
 
-parser = argparse.ArgumentParser(prog='assign')
+p = argparse.ArgumentParser(prog='assign')
 
-parser.add_argument('--path', '-p', default=TAR_CACHE_PATH,
-                    help='The path on your system to set the scope of file search to (Default: %(default)s).')
+p.add_argument('--path', '-p', default=TAR_CACHE_PATH,
+               help='The path on your system to set the scope of file search to (Default: %(default)s).')
 
-parser.add_argument('--small_path', '-s', default=SMALL_TAR_CACHE_PATH,
-                    help='The path on your system to set the scope of file search to (Default: %(default)s).')
+p.add_argument('--small_path', '-s', default=SMALL_TAR_CACHE_PATH,
+               help='The path on your system to set the scope of file search to (Default: %(default)s).')
 
-parser.add_argument('--debug', '-d', action='store_true',
-                    help='If included, the program will print info throughout the process (Default: %(default)s).')
+p.add_argument('--debug', '-d', action='store_true', default=s.DEFAULT_DEBUG,
+               help='If included, the program will print info throughout the process (Default: %(default)s).')
 
-parser.add_argument('--verbosity', '-v', type=int, default=1,
-                    help='Changes the log / debug message verbosity. Not all functions may be affected by this '
+p.add_argument('--verbosity', '-v', type=int, default=s.DEFAULT_VERBOSITY,
+               help='Changes the log / debug message verbosity. Not all functions may be affected by this '
                          'value! Possible values are ... '
                          '0 = only errors, '
                          '1 = low, '
@@ -38,22 +38,36 @@ parser.add_argument('--verbosity', '-v', type=int, default=1,
                          '3 = high '
                          '(Default: %(default)s).')
 
+subparsers = p.add_subparsers()
+
+pt = subparsers.add_parser(name='tag', help='tagging help')
+
 # Add custom OPTIONS to the script when running command-line
-OPTIONS: argparse.Namespace = parser.parse_args()
+OPTIONS: argparse.Namespace = p.parse_args()
 
 assigner_cache = os.path.join(OPTIONS.path, ASSIGNER_FILE_NAME)
 
 assigner: ImageAssigner
 
-if os.path.exists(assigner_cache):
-    with open(assigner_cache, 'rb') as f:
-        assigner = pickle.load(f)
-        if OPTIONS.debug:
-            print('Using existing assigner object at ' + OPTIONS.path + ' ... ')
-else:
+if os.path.exists(assigner_cache) is False:
     if OPTIONS.debug:
         print('Creating new assigner object at ' + OPTIONS.path + ' ... ')
     assigner = ImageAssigner(scope_path=OPTIONS.path,
-                             small_path=OPTIONS.small_path)
+                             small_path=OPTIONS.small_path,
+                             debug=OPTIONS.debug,
+                             verbosity=OPTIONS.verbosity)
 
-pickle.dump(assigner, open(assigner_cache, 'wb'), pickle.HIGHEST_PROTOCOL)
+    cache_data = jsonpickle.encode(assigner)
+    with open(assigner_cache, 'w') as f:
+        f.write(cache_data)
+
+with open(assigner_cache, 'r') as f:
+    assigner = jsonpickle.decode(f.read())
+    if OPTIONS.debug:
+        print('Using assigner object at ' + OPTIONS.path + ' ... ')
+
+# Modification of object here
+
+cache_data = jsonpickle.encode(assigner)
+with open(assigner_cache, 'w') as f:
+    f.write(cache_data)
--- src/python/Poststorm_Imagery/assigner/image_assigner.py
+++ src/python/Poststorm_Imagery/assigner/image_assigner.py
@@ -30,15 +30,21 @@ class ImageAssigner:
     catalog_path: Union[bytes, str]  # The path to the catalog file
     small_path: Union[bytes, str, None]  # The path to the resized image scope path
 
-    pending_images_queue: Queue = Queue()  # The queue that stores all images left to tag by their ID
-    finished_tagged_queue: LifoQueue = LifoQueue()  # The queue to store images that have been tagged already
-    max_skipped_queue: LifoQueue = LifoQueue()  # The queue of images that have passed the threshold for max skips
+    pending_images_queue: List[Image] = []  # The queue that stores all images left to tag by their ID
+    finished_tagged_queue: List[Image] = []  # The queue to store images that have been tagged already
+    max_skipped_queue: List[Image] = []  # The queue of images that have passed the threshold for max skips
 
     # Each user's current image once removed from the beginning of the image queue
     current_image: Dict[str, Image] = {}
 
     def __init__(self, scope_path: Union[bytes, str],
-                 small_path: Union[bytes, str, None] = None):
+                 small_path: Union[bytes, str, None] = None, **kwargs):
+
+        # Enable debugging flag (True = output debug statements, False = don't output debug statements)
+        debug: bool = (kwargs['debug'] if 'debug' in kwargs else s.DEFAULT_DEBUG)
+
+        # Enable verbosity (0 = only errors, 1 = low, 2 = medium, 3 = high)
+        verbosity: int = (kwargs['verbosity'] if 'verbosity' in kwargs else s.DEFAULT_VERBOSITY)
 
         self.scope_path = h.validate_and_expand_path(scope_path)
 
@@ -57,8 +63,12 @@ class ImageAssigner:
 
         # Add each image into the queue
         for image in self.image_list_from_csv():
-            self.pending_images_queue.put(image)
-            self.pending_images_queue.task_done()
+            self.pending_images_queue.append(image)
+            if debug and verbosity >= 1:
+                print('Loaded %s from the %s.csv file' % (str(image), s.CATALOG_FILE_NAME))
+
+        if debug and verbosity >= 1:
+            print('Next Pending Image (of %s): %s' % (len(self.pending_images_queue), self.pending_images_queue[0]))
 
     def image_list_from_csv(self) -> List[Image]:
 
@@ -80,8 +90,7 @@ class ImageAssigner:
             return self.current_image[user_id].small_size_path
 
     def get_next_image_path(self, user_id: str, full_size: bool = False) -> str:
-        self.current_image[user_id] = self.pending_images_queue.get()
-        self.pending_images_queue.task_done()
+        self.current_image[user_id] = self.pending_images_queue.pop()
 
         if full_size:
             return self.current_image[user_id].original_size_path
@@ -98,14 +107,13 @@ class ImageAssigner:
         return self.current_image[user_id]
 
     def get_next_image(self, user_id: str) -> Image:
-        next_image: Image = self.pending_images_queue.get()
+        next_image: Image = self.pending_images_queue.pop()
 
         if user_id not in (next_image.skippers and next_image.taggers):
             self.current_image[user_id] = next_image
-            self.pending_images_queue.task_done()
         else:
             next_next_image = self.get_next_image(user_id=user_id)
-            self.pending_images_queue.put(next_image)
+            self.pending_images_queue.insert(0, next_image)
 
             return next_next_image
 
@@ -115,19 +123,16 @@ class ImageAssigner:
         self.current_image[user_id].add_tag(user_id=user_id, tag=tag, content=content)
 
     def user_done_tagging_current_image(self, user_id: str):
-        self.finished_tagged_queue.put(self.current_image[user_id])
-        self.finished_tagged_queue.task_done()
+        self.finished_tagged_queue.append(self.current_image[user_id])
 
     def user_skip_tagging_current_image(self, user_id: str):
 
         self.current_image[user_id].skippers.append(user_id)
 
         if len(self.current_image[user_id].skippers) > MAX_SKIP_THRESHOLD:
-            self.max_skipped_queue.put(self.current_image[user_id])
-            self.max_skipped_queue.task_done()
+            self.max_skipped_queue.append(self.current_image[user_id])
         else:
-            self.pending_images_queue.put(self.current_image[user_id])
-            self.pending_images_queue.task_done()
+            self.pending_images_queue.append(self.current_image[user_id])
 
 
 class CatalogNotFoundException(IOError):
