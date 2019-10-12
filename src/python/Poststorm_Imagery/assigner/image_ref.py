3c4e7acbaeffd904156c5015a69dadc6583025b1
Matmorcat <mattmoretz@yahoo.com>
2019-10-12 00:13:38 -0400

    Implement function to find the best set of tags for an image based on users who have tagged the image (majority wins). Returns None if no consensus

--- src/python/Poststorm_Imagery/assigner/image_ref.py
+++ src/python/Poststorm_Imagery/assigner/image_ref.py
@@ -6,6 +6,26 @@ from typing import Dict, Union, Set
 from Poststorm_Imagery import h
 
 
+def _cast_valid_types(content: str) -> Union[str, bool, int]:
+    """
+    Cast an input that explicitly reads "true" or "false" (case-insensitive) as a boolean type and cast all strings
+    of only digits as an integer type.
+
+    :param content: The string of content to parse out compatible types for
+    :return: The value casted as the type detected
+    """
+
+    # Check if the response matches a boolean's text (must be explicit to prevent coercion of ints like '1' -> True)
+    if content.lower() == ('true' or 'false'):
+        content = bool(content)
+
+    # Check if the response is an integer and only an integer (explicitly define match to avoid type coercion)
+    elif re.fullmatch('\\d+', content):
+        content = int(content)
+
+    return content
+
+
 class Image:
 
     original_size_path: Union[bytes, str]  # The relative path from the catalog.csv for the full size image
@@ -44,13 +64,8 @@ class Image:
         if user_id not in self.get_taggers():
             self.taggers[user_id] = dict()
 
-        # Check if the response matches a boolean's text (must be explicit to prevent coercion of ints like '1' -> True)
-        if content.lower() == ('true' or 'false'):
-            content = bool(content)
-
-        # Check if the response is an integer and only an integer (explicitly define match to avoid type coercion)
-        elif re.fullmatch('\\d+', content):
-            content = int(content)
+        # If a valid type is found in the string, cast as either bool or int, otherwise keep as a string
+        content = _cast_valid_types(content=content)
 
         self.taggers[user_id][tag]: Union[str, bool, int] = content
 
@@ -147,6 +162,7 @@ class Image:
 
         :return: Whether (True) or not (False) all users' non-string tags have equal values
         :except NotEnoughTaggersError: The number of current taggers for this image is less than 2
+        :except UsersHaveDifferentTagsError: There is an issue with saving user's tags (incomplete or inconsistent)
         """
 
         # Make sure there are at least 2 sets of tags to compare (function should not be called otherwise)
@@ -158,14 +174,73 @@ class Image:
 
         for tag in tag_totals:
 
-            # If there are users with different responses for a non-string tag
             if len(tag_totals[tag].keys()) > 2:
+                # If there are users with different responses for a non-string tag
                 return False
+
+            agreed_upon_value = list(tag_totals[tag].keys())[0]
+            if tag_totals[tag][agreed_upon_value] != len(self.get_taggers()):
+                # If one user has non-string tags added that the other doesn't
+                raise UsersHaveDifferentTagsError
 
         # All tags have exactly one value for each tag name
         return True
+
+    def get_best_tags(self) -> Dict[str, Union[bool, int, str]] or None:
+        """
+        This function allows you to get the tags that have the most user's opinions on tags in common. If there is a
+        clear winner for each tag, then this function returns the set of winning tags. If there is any ambiguity in
+        which tag should be chosen (i.e. all people who tagged the image chose different tags), then the function
+        will return None. In the case of None, the image should probably have an additional person tag it then
+        re-checked to ensure there is a majority for one value for each tag.
+
+        :return: A dictionary of tags as keys and their values as booleans, integers, or strings with no reference to
+        the original users who tagged them as such. If there is some ambiguity, it returns None.
+        """
+
+        tags_flattened: Dict[str, Union[bool, int, str]] = dict()  # tags_flattened[tag] = <value>
+
+        # Create a dictionary that contains all the image's final tags and values.
+
+        if self.all_user_tags_equal():
+            first_user = list(self.get_taggers())[0]
+            for tag, value in self.taggers[first_user].items():
+                if type(value) is not str:
+                    # Add all non-string tags to the result of tags found
+                    tags_flattened[tag] = value
+
+        else:
+            tag_totals: Dict[str, Dict[str, int]] = self._sum_tag_values_by_users()  # tag_totals[tag][value] = <number of user_ids>
+
+            for tag in tag_totals:
+
+                all_max_value_names: Set[str] = set()
+
+                # Get the first key of the value name with the most occurrences
+                max_value_name = max(tag_totals[tag], key=tag_totals[tag].get)
+                for value_name, count in tag_totals.items():
+                    if count == tag_totals[max_value_name]:
+
+                        # Make a set of all tag values that have equally high amount of same values for a tag
+                        all_max_value_names.add(value_name)
+
+                if len(all_max_value_names) == 1:
+                    # There exists only one value for a tag that has the most votes (i.e. 2 of 3 people agree)
+                    tags_flattened[tag] = _cast_valid_types(content=list(all_max_value_names)[0])
+
+                else:
+                    # There is no clear winner for this tag
+                    return None
+
+        return tags_flattened
 
 
 class NotEnoughTaggersError(ValueError):
     def __init__(self):
         Exception.__init__(self, 'Tried to compare taggers\' tags when there were less than 2!')
+
+
+class UsersHaveDifferentTagsError(ValueError):
+    def __init__(self):
+        Exception.__init__(self, 'One or more users has missing values for tags even though their submission was '
+                                 'considered complete! This is likely due to an error in the front-end submission.')
