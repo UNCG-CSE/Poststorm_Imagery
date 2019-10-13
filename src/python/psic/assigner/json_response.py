cec540a73aad952831aa2721dcd648182aef85bb
Matmorcat <mattmoretz@yahoo.com>
2019-10-12 20:42:19 -0400

    Fix some linting problems

--- src/python/psic/assigner/json_response.py
+++ src/python/psic/assigner/json_response.py
@@ -16,13 +16,13 @@ class JSONResponse:
 
     def __init__(self, status: int, content=None, error_message: str = None):
         self.status = status
-        if self.status is 0:
+        if self.status == 0:
             self.content = content
-        elif self.status is 1:
+        elif self.status == 1:
             self.error_message = error_message
         else:
             self.status = 1
-            if error_message is None or len(error_message) is 0:
+            if error_message is None or len(error_message) == 0:
                 self.error_message = 'Unknown error in json response object input'
             else:
                 self.error_message = error_message
