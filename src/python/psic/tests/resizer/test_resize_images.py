2f354681498e8e4e0fed146743e35f8d9aee97b1
Matmorcat <mattmoretz@yahoo.com>
2019-10-13 21:08:19 -0400

    Isolate a copy of the data in its own environment

--- src/python/psic/tests/resizer/test_resize_images.py
+++ src/python/psic/tests/resizer/test_resize_images.py
@@ -5,21 +5,24 @@ import pytest
 from psic.resizer.generate import ResizeImages
 
 SELF_PATH = os.path.dirname(os.path.abspath(__file__))
+INPUT_PATH = os.path.join(SELF_PATH, 'input')
+EXPECTED_PATH = os.path.join(SELF_PATH, 'expected')
+OUTPUT_PATH = os.path.join(SELF_PATH, 'output')
 
 
 class TestResizeImages(unittest.TestCase):
 
     # Allow for capturing console output for comparison using pytest fixtures
     @pytest.fixture(autouse=True)
-    def _pass_fixtures(self, capfd):
+    def _pass_fixtures(self, capfd, shared_datadir):
         self.capfd = capfd
+        self.shared_datadir = shared_datadir
 
     def test_resize_all_images_debug(self):
 
         expected_size: tuple = (1081, 811)
-
-        images = ResizeImages.resize_all_images(path=os.path.join(SELF_PATH, 'input'),
-                                                output_path=os.path.join(SELF_PATH, 'output'),
+        images = ResizeImages.resize_all_images(path=str(self.shared_datadir / 'input'),
+                                                output_path=str(self.shared_datadir / 'output'),
                                                 scale=0.15,
                                                 save=True,
                                                 debug=True)
@@ -31,5 +34,5 @@ class TestResizeImages(unittest.TestCase):
         print('OUTPUT: ' + out)
         assert 'Searching through' in str(out)
         assert 'input for the pattern' in str(out)
-        assert 'Resizing file 1 of 2 (50.00%): C26347592.jpg' in str(out)
-        assert 'Resizing file 2 of 2 (100.00%): C26347598.jpg' in str(out)
+        assert 'Resizing file 1 of 2 (50.00%): ' in str(out)
+        assert 'Resizing file 2 of 2 (100.00%): ' in str(out)
