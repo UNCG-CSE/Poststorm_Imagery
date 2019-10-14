b0f1883adeaf49ac0c6e2cea6e56e673a014e111
Matmorcat <mattmoretz@yahoo.com>
2019-10-13 21:30:27 -0400

    Datadir only seems to work for Windows OS

--- src/python/psic/tests/resizer/test_resize_images.py
+++ src/python/psic/tests/resizer/test_resize_images.py
@@ -5,9 +5,10 @@ import pytest
 from psic.resizer.generate import ResizeImages
 
 SELF_PATH = os.path.dirname(os.path.abspath(__file__))
-INPUT_PATH = os.path.join(SELF_PATH, 'input')
-EXPECTED_PATH = os.path.join(SELF_PATH, 'expected')
-OUTPUT_PATH = os.path.join(SELF_PATH, 'output')
+DATA_PATH = os.path.join(SELF_PATH, 'data')
+INPUT_PATH = os.path.join(DATA_PATH, 'input')
+EXPECTED_PATH = os.path.join(DATA_PATH, 'expected')
+OUTPUT_PATH = os.path.join(DATA_PATH, 'output')
 
 
 class TestResizeImages(unittest.TestCase):
@@ -16,13 +17,16 @@ class TestResizeImages(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def _pass_fixtures(self, capfd, shared_datadir):
         self.capfd = capfd
-        self.shared_datadir = shared_datadir
+        if os.name == 'nt':
+            self.shared_datadir = str(shared_datadir)
+        else:
+            self.shared_datadir = str(DATA_PATH)
 
     def test_resize_all_images_debug(self):
 
         expected_size: tuple = (1081, 811)
-        images = ResizeImages.resize_all_images(path=str(self.shared_datadir / 'input'),
-                                                output_path=str(self.shared_datadir / 'output'),
+        images = ResizeImages.resize_all_images(path=os.path.join(self.shared_datadir, 'input'),
+                                                output_path=os.path.join(self.shared_datadir, 'output'),
                                                 scale=0.15,
                                                 save=True,
                                                 debug=True)
@@ -31,7 +35,7 @@ class TestResizeImages(unittest.TestCase):
             assert image.size == expected_size
 
         out, err = self.capfd.readouterr()
-        print('OUTPUT: ' + out)
+        print('OUTPUT: ' + str(out).replace('\r', '\\r\n'))
         assert 'Searching through' in str(out)
         assert 'input for the pattern' in str(out)
         assert 'Resizing file 1 of 2 (50.00%): ' in str(out)
