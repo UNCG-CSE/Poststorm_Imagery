511e6cf7613ad416dbecc1cef7baa76f160d25fe
Matmorcat <mattmoretz@yahoo.com>
2019-10-29 22:10:05 -0400

    Allow support for no sub-command and fix shadowing of psic module

--- src/python/psic/main/pstorm.py
+++ src/python/psic/main/pstorm.py
@@ -14,12 +14,15 @@ paths = {
     'resize': '../resizer/resize.py'
 }
 
-if len(sys.argv) > 1 and sys.argv[1] in paths.keys():
+if len(sys.argv) >= 2 and sys.argv[1] in paths.keys():
     script = h.validate_and_expand_path(os.path.join(SELF_PATH, paths[sys.argv[1]]))
     sys.argv.remove(sys.argv[1])
     sys.argv[0] = str(script)
     exec(open(sys.argv[0]).read())
+elif len(sys.argv) >= 2:
+    print('Unknown sub-command "%s".'
+          '\nValid sub-commands are %s'
+          % (sys.argv[1], ', '.join(paths)))
 else:
-    h.print_error('Unknown sub-command "%s".'
-                  '\nValid sub-commands are %s'
-                  % (sys.argv[1], ', '.join(paths)))
+    print('Usage: %s [%s]'
+          % (sys.argv[0], '|'.join(paths)))
