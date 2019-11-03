811a21f745ebae9c5e4902d95604645508ccddd2
Matmorcat <mattmoretz@yahoo.com>
2019-11-03 14:31:20 -0500

    Standardize fields with path and debug

--- src/python/psic/assigner/batch.py
+++ src/python/psic/assigner/batch.py
@@ -1,18 +1,23 @@
 from typing import Union, List
 
+from psic import s
+
 
 class Batch:
 
-    scope_path: Union[bytes, str]  # The path of where to find the data and catalog.csv
+    path: Union[bytes, str]  # The path of where to find the data and catalog.csv
     small_path: Union[bytes, str]  # The path to the resized image scope path
     operations: List[dict]         # An ordered list of operations to execute
 
-    def __init__(self, scope_path: Union[bytes, str], small_path: Union[bytes, str],
-                 operations: List[dict] or None = None):
+    debug: bool  # True for debug statements
+
+    def __init__(self, path: Union[bytes, str], small_path: Union[bytes, str],
+                 operations: List[dict] or None = None, debug: bool = s.DEFAULT_DEBUG):
 
         if operations is None:
             operations = list()
 
-        self.scope_path = scope_path
+        self.scope_path = path
         self.small_path = small_path
         self.operations = operations
+        self.debug = debug
