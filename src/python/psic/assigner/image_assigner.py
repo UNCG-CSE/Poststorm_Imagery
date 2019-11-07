5c0b137e8ceb40720981dd97cd374035a70f190e
Matmorcat <mattmoretz@yahoo.com>
2019-11-07 18:21:34 -0500

    Fix if user not having an image, assigning and not removing from queue

--- src/python/psic/assigner/image_assigner.py
+++ src/python/psic/assigner/image_assigner.py
@@ -124,10 +124,7 @@ class ImageAssigner:
         # If the user has no current image, assign them one from the pending queue
 
         if user_id not in self.current_image.keys():
-            self.current_image[user_id] = self._get_next_suitable_image(user_id=user_id)
-
-            # Record that the user started tagging the image
-            self.current_image[user_id].stats_tagging_start[user_id]: float = datetime.now().timestamp()
+            self.get_next_image(user_id=user_id)
 
         if expanded:
             return self.current_image[user_id].expanded(scope_path=self.scope_path, small_path=self.small_path)
