3d5c562d63137759cbdcf887caabbea187627231
ShahNafisRafique <sarafiqu@uncg.edu>
2019-11-13 15:32:43 -0500

    fixed file path for the tagged data json and made functions

--- src/python/psic/stats/tagging_stats/main.py
+++ src/python/psic/stats/tagging_stats/main.py
@@ -2,18 +2,26 @@ import pandas as pd
 import jsonpickle
 import numpy as np
 
-JSON_FILE_TO_IMPORT = "../../../../../../tagging_data_personal.json"  # "test.json"
+JSON_FILE_TO_IMPORT = "../tagging_data_personal.json"  # "test.json"
 
-with open(JSON_FILE_TO_IMPORT, 'r') as f:
-    data = jsonpickle.decode(f.read())
 
-    done_tagging_count=len(data.finished_tagged_queue)
+def get_pickle(path_to_file):
+    with open(JSON_FILE_TO_IMPORT, 'r') as f:
+        return jsonpickle.decode(f.read())
+        
+def how_many_taggged(pickle_file):
+    done_tagging_count=len(pickle_file.finished_tagged_queue)
 
     tagged_but_not_done_count=0
-    for image in data.pending_images_queue:
+    for image in pickle_file.pending_images_queue:
         if len(image.get_taggers()) > 0:
             tagged_but_not_done_count-=-1
-    
+
     print(f"{tagged_but_not_done_count} images have been tagged, but not completely")
     print(f"{done_tagging_count} images have been tagged completely")
     print(f"{tagged_but_not_done_count+done_tagging_count} images have been tagged, completely or not")
+
+data = get_pickle(JSON_FILE_TO_IMPORT)
+
+print(data.pending_images_queue[-1].taggers)
+
